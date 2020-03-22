80d4dbc6cf4097ebef68e515cbbeba3957cd0858
gengjian1203 <gengjian1203@foxmail.com>
2020-03-22 17:01:56 +0800

    更新了更新成员信息的接口和存储字段

--- updateMemeber/index.js
+++ updateMemeber/index.js
@@ -6,59 +6,105 @@ cloud.init();
 // 可在入口函数外缓存 db 对象
 const db = cloud.database();
 
+// 创建成员信息
+createMember = async (openId, newInfo) => {
+  let data = {};
+  // 系统级
+  data._openid = openId;
+  data.createTime = db.serverDate();    // 创建时间
+  data.loginTime = db.serverDate();    // 创建时间
+  data.updateTime = db.serverDate();    // 修改时间
+  // 外部展示
+  data.avatarUrl = newInfo.avatarUrl; // 头像url
+  data.nickName = newInfo.nickName; // 姓名
+  data.level = 1; // 等级
+  data.exp = 0; // 经验
+  data.vip = 0; // 会员等级
+  data.vip_exp = 0; // 会员经验
+  data.money = 0; // 铜钱
+  data.gold = 0; // 元宝
+  // 属性展示
+  data.hp = 100; // 生命
+  data.attack = 20; // 攻击
+  data.defense = 0; // 防御
+  data.dodge = 0; // 闪避
+
+  // 创建新的用户记录
+  try {
+    await db.collection('member').add({
+      data
+    });
+  } catch (e) {
+    console.log('createMember error.', e);
+  }
+}
+
+// 更新成员信息
+updateMemberInfo = async (openId, newInfo, oldInfo, isLogin) => {
+  let data = oldInfo;
+  // 除去id与openid
+  delete data['_id'];
+  delete data['_openid'];
+
+  // 更新操作时间
+  if (isLogin) {
+    data.loginTime = db.serverDate();    // 登录时间
+  } 
+  data.updateTime = db.serverDate();   // 更新时间
+  // 将待更新数据放入data
+  for (let key in newInfo) {
+    data[key] = newInfo[key];
+  }
+
+  // 更新用户信息
+  try {
+    await db.collection('member')
+            .where({
+              _openid: openId
+            })
+            .update({
+              data
+            });
+  } catch (e) {
+    console.log('updateMemberInfo error.', e);
+  }
+}
+
 // 云函数入口函数
 exports.main = async (event, context) => {
   // 
   const openId = cloud.getWXContext().OPENID;
+  const newInfo = event.memberInfo;
+  const isLogin = event.isLogin;
   let result = true;
-  let memberInfo = null;
+  let oldInfo = null;
 
   // 确认是否有数据
   try {
-    memberInfo = await db.collection('member').doc(openId).get();
+    oldInfo = await db.collection('member')
+                         .where({
+                           _openid: openId
+                         }).get();
   } catch(e) {
     // 之前没有数据
-    console.log('query memberInfo error.', e);
+    console.log('main query oldInfo error.', e);
   }
+
+  // 进行处理
   try {
-    console.log('memberInfo', memberInfo);
-    if (!memberInfo) {
-      // 创建新的用户记录
-      await db.collection('member').add({
-        data: {
-          // 不指定_id，数据库会默认生成一个
-          // 用户的openId
-          _openid: openId,
-          // 外部展示
-          // 等级
-          level: 1,
-          // 经验
-          exp: 0,
-          // 会员等级
-          vip: 0,
-          // 会员经验
-          vip_exp: 0,
-          // 铜钱
-          money: 0,
-          // 元宝
-          gold: 0,
-          // 属性展示
-          // 生命
-          hp: 100,
-          // 攻击
-          attack: 20,
-          // 防御
-          defense: 0,
-          // 闪避
-          dodge: 0,
-        }
-      });
+    if (oldInfo.data.length === 0) {
+      // 创建用户信息
+      console.log('createMember oldInfo', oldInfo)
+      await createMember(openId, newInfo);
     } else {
       // 更新用户信息
+      console.log('updateMemberInfo oldInfo', oldInfo)
+      console.log('updateMemberInfo newInfo', newInfo)
+      await updateMemberInfo(openId, newInfo, oldInfo.data[0], isLogin);
     }
   } catch (e) {
     result = false;
-    console.log('update memberInfo error.', e);
+    console.log('main update memberInfo error.', e);
   }
 
   return {
